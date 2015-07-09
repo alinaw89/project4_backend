4e1641d9b6d1b052a6f282d7962b1998e5b7ff7a
alinaw89 <ali.naw89@gmail.com>
2015-07-09 17:17:32 -0400

    adds specs in apiconstraintsspec.rb and adds api_constraints.rb, also adds the class to routes.rb and set it as constraint scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api'}, path: '/' do
-    scope module: :v1 do
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
     end
   end
