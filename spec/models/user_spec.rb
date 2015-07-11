0e5d6235b423d5a5adedad2d4a519d07b04d0826
alinaw89 <ali.naw89@gmail.com>
2015-07-11 16:51:37 -0400

    fixes capybara gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
+require 'capybara'
+include Capybara::DSL
 
 describe "Viewing the list of users" do
-  it "shows the user"
+  it "shows the user" do
+    visit "http://example.com/users"
+
+    expect(page).to have_text("4 Users")
+    expect(page).to have_text("User 1")
+    expect(page).to have_text("User 2")
+    expect(page).to have_text("User 3")
+    expect(page).to have_text("User 4")
+  end
 end
