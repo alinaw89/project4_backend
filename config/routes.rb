e71a98110b91417f04e19e92087211d3a923bdb1
alinaw89 <ali.naw89@gmail.com>
2015-07-11 16:58:02 -0400

    creates first test for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,9 @@
-# require 'api_constraints'
-
-# Rails.application.routes.draw do
-#   # API definition
-#   namespace :api, defaults: { format: :json },
-#                               constraints: { subdomain: 'api'}, path: '/' do
-#     scope module: :v1,
-#       constraints: ApiConstraints.new(version: 1, default: true) do
-#       # We are going to list our resources here
-#     end
-#   end
-# end
-
 Rails.application.routes.draw do
 
-  # root 'cosmetic_products#index'
-   # get '/cosmetic_products' => 'cosmetic_products#index'
-
-   # get '/users' => 'users#index'
-
-   # post '/login' => 'auth#login'
-   # post '/register' => 'auth#register'
-   # delete '/logout' => 'auth#logout'
 
    # root "users#index"
 
-
+   get "users" => "users#index"
    # resources :users, except: [:new, :edit] do
 
    # end
