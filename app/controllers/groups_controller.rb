a66006faa0446ab217417fb59b84bfa41fed6636
alinaw89 <ali.naw89@gmail.com>
2015-07-11 22:54:58 -0400

    adds actions to groups and memberships controllers

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,41 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+    render json: @groups
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    render json: @group
+  end
+
+  def create
+    @group = current_user.groups.new(groups_params)
+    if @group.save
+      render json: @group, status: :created, location: groups_url
+    else
+      render json: @group.errors, status: :unprocessable_entity
+    end
+  end
+
+  # def update
+  #   @group = Group.find(params[:id])
+  #   if @group(groups_params)
+  #     head :no_content
+  #   else
+  #     render json: @group.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    head :no_content
+  end
+
+  private
+  def groups_params
+    params.permit(:name)
+  end
+
+end
