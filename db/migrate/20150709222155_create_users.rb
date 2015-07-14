e51af6f68795e002c4a7a66d81ec1afbb2ac81cb
alinaw89 <ali.naw89@gmail.com>
2015-07-14 10:33:26 -0400

    adds columns to user model for email, password digest, and token

--- db/migrate/20150709222155_create_users.rb
+++ db/migrate/20150709222155_create_users.rb
@@ -2,8 +2,10 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
+      t.string :email, null:false, index: { unique:true }
       t.string :phone_number
+      t.string :password_digest, null:false
+      t.string :token, null:false, index: { unique:true }
 
       t.timestamps null: false
     end
